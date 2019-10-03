9ac9581db7f4db326e12fc8d3306197ccf6ff630
Ivan Simonini <gods@roundhousecode.com>
2019-10-03 20:00:30 +0200

    Working on a different test

---
+++ src/test/java/com/roundhousecode/gods/thing/ValidatorMockTest.java
@@ -0,0 +1,36 @@
+package com.roundhousecode.gods.thing;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(CamelSpringBootRunner.class)
+@SpringBootTest()
+public class ValidatorMockTest {
+
+    @Autowired
+    private CamelContext camelContext;
+
+    @Produce(uri="simple:start")
+    private ProducerTemplate producer;
+
+    @EndpointInject(uri="mock:result")
+    private MockEndpoint endpoint;
+
+    @Test
+    public void test() throws InterruptedException {
+
+        endpoint.expectedHeaderReceived("lol", false);
+
+        producer.sendBodyAndHeader("this is a body", "lol", false);
+
+        endpoint.assertIsSatisfied();
+    }
+}
